d3f31954d95faa5270f4b0b52d5d6c4fa36dd2c8
John <johnel21@outlook.com>
2025-09-15 21:43:18 -0300

    commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 export async function Consultar(){
     const consulta = await fetch(import.meta.env.VITE_URL)
+    console.log(import.meta.env.VITE_URL);
+    
     const {items} = await consulta.json()
     return items ?? []
 }
